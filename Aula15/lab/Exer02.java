8d192132eeb42b8cec483742ddf9d4fa157276ad
juniorribeiro89 <57049828+juniorribeiro89@users.noreply.github.com>
2020-06-18 01:30:38 -0300

    .
    .

--- Aula15/lab/Exer02.java
+++ Aula15/lab/Exer02.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.loiane.cursojava.aula15.lab;
+package com.loiane.cursojava.AulasLoiane.ExerciciosLoiane.Aula15.lab;
 
 import java.util.Scanner;
 
